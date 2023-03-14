c2c919efdf0b3230e515bccdc508098a9caf6059
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2023-03-14 17:04:35 -0400

    commit tree 8865384cef328d5faafb404183cded2b7feb6407

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
+import thunkMiddleware from "redux-thunk";
+import { cmoposedWithDevTools } from "redux-devtools-extension";
 import App from "./App";
 import rootReducer from "./reducer";
+import { applyMiddleware } from "redux";
+
+const composedEnhancer = cmoposedWithDevTools(applyMiddleware(thunkMiddleware));
 
 const store = createStore(
   rootReducer,
+  composedEnhancer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
